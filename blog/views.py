458899391a23d8b068bc83818af555dc7f5027ea
sim0n89 <52407038+sim0n89@users.noreply.github.com>
2023-07-21 08:40:57 +0300

    add post page layout.

--- blog/views.py
+++ blog/views.py
@@ -48,12 +48,12 @@ def post_detail(request, slug):
             'published_at': comment.published_at,
             'author': comment.author.username,
         })
-
     serialized_post = {
         "title": post.title,
         "text": post.text,
         "author": post.author.username,
         "comments": serialized_comments,
+        "comments_amount": len(serialized_comments),
         'likes_amount': post.likes.count(),
         "image_url": post.image.url if post.image else None,
         "published_at": post.published_at,
